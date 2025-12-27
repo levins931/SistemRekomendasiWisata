4c55e2332be6cf87750d2dc3844b699ae96e3105
levins931 <samsularifin110923@gmail.com>
2025-12-27 15:30:03 +0700

    configure settings for render deployment

--- sistem_rekomendasi/settings.py
+++ sistem_rekomendasi/settings.py
@@ -24,10 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r4gk-gk#pgui9djo7b$u$s*v8%s=b!f^4g92ba)+qwtj%ayinq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
 
 # Application definition
 
@@ -45,6 +48,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -52,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'sistem_rekomendasi.urls'
 
@@ -137,4 +143,8 @@ UNFOLD = {
     "SHOW_HISTORY": True,
     "SHOW_PROFILE": True,
 }
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
